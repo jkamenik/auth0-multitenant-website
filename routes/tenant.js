9cebd94ada979a1973017b0fa15524aa6f01fa7a
John Kamenik <jkamenik@fourv.com>
2016-12-12 08:56:10 -0500

    Filter out virtual tenants

--- routes/tenant.js
+++ routes/tenant.js
@@ -5,19 +5,24 @@ var router = express.Router();
 
 function buildTenants (req) {
   return req.user._json.groups.map(tenant => {
+    var isVirtual = tenant.startsWith("region-");
+
     return {
       name: tenant,
+      isVirtual: isVirtual,
       url: `http://${tenant}.${process.env.ROOT_DOMAIN}:${process.env.PORT}/user`
     };
+  }).filter(t => {
+    return !t.isVirtual;
   });
 }
 
 /* GET tenant chooser page */
-router.get('/choose', 
-  ensureLoggedIn(), 
+router.get('/choose',
+  ensureLoggedIn(),
   function(req, res, next) {
-    res.render('select_tenant', { 
-      user: req.user, 
+    res.render('select_tenant', {
+      user: req.user,
       tenants: buildTenants(req),
       title: 'Choose a tenant',
       message: "Oh no! I can't figure out where to send you.  Please tell me where you want to go."
@@ -25,11 +30,11 @@ router.get('/choose',
   });
 
 /* GET user unauthorized for tenant page */
-router.get('/unauthorized', 
-  ensureLoggedIn(), 
+router.get('/unauthorized',
+  ensureLoggedIn(),
   function(req, res, next) {
-    res.render('select_tenant', { 
-      user: req.user, 
+    res.render('select_tenant', {
+      user: req.user,
       tenants: buildTenants(req),
       title: 'Unauthorized',
       message: "Sorry, you're not authorized to access that tenant. Please choose another."
