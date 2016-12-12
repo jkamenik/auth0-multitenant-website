44c29f7187a30bf9c82f6c4039adb00fb3e3c6ce
John Kamenik <jkamenik@fourv.com>
2016-12-12 08:46:48 -0500

    Skipping virtual tenants
    Also added full user details for debugging.

--- routes/user.js
+++ routes/user.js
@@ -6,24 +6,28 @@ var router = express.Router();
 var tenant = require('../lib/tenant');
 
 /* GET user profile. */
-router.get('/', 
+router.get('/',
   ensureLoggedIn(),
   tenant.setCurrent(),
   tenant.ensureCurrent(),
-  tenant.ensureUrl(), 
+  tenant.ensureUrl(),
   function(req, res) {
     var tenants = req.user._json.groups.map(tenant => {
       var isActive = tenant === req.tenant;
+      var isVirtual = tenant.startsWith("region-");
 
       return {
         name: tenant,
         isActive: isActive,
+        isVirtual: isVirtual,
         url: isActive ? '#' : `http://${tenant}.${process.env.ROOT_DOMAIN}:${process.env.PORT}/user`
       };
+    }).filter(function(tenant){
+      return !tenant.isVirtual;
     });
 
-    res.render('user', { 
-      user: req.user, 
+    res.render('user', {
+      user: req.user,
       tenants: tenants,
       currentTenant: req.tenant
     });
